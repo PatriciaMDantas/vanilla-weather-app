50b5df46db235b3a15e554c713444a7d58bf13c7
PatriciaMDantas <patricia87dantas@gmail.com>
2022-05-05 13:50:51 +0100

    add elements: humidity, wind, information

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#Temperature");
   let cityElement = document.querySelector("#City");
+  let informationElement = document.querySelector("#information");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innetHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   informationElement.innerHTML = response.data.weather[0].information;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "f727ec0dfcbdd24b05a503781a2f00e8";
